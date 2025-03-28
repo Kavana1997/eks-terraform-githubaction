6933e2568c2b013d736fa20018eb7917fd7c7ad7
Kavana1997 <102670832+Kavana1997@users.noreply.github.com>
2025-03-28 11:57:23 +0530

    Create version.tf

---
+++ version.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+  }
+  
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "eks/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
